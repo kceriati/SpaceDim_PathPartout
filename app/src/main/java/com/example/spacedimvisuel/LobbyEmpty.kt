e01a52d5c857a8c8a5ce7c7de8581d7922d35d35
charlottedrb <c.derbaghdassarian@gmail.com>
2021-01-03 16:24:32 +0100

    ecran lobby en cours - principal DONE - WIP liste des joueurs

---
+++ app/src/main/java/com/example/spacedimvisuel/LobbyEmpty.kt
@@ -0,0 +1,11 @@
+package com.example.spacedimvisuel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LobbyEmpty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lobby_empty)
+    }
+}
