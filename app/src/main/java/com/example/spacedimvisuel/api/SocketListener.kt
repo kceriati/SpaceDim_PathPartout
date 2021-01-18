f06d883298801ce53970c8d01743004ec36a2406
YoanRouleau <yoan-rouleau@live.fr>
2021-01-18 16:19:24 +0100

    LoginViewModel -> MainActivity -> LobbyViewModel

--- app/src/main/java/com/example/spacedimvisuel/api/SocketListener.kt
+++ app/src/main/java/com/example/spacedimvisuel/api/SocketListener.kt
@@ -5,21 +5,3 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 
-class SocketListener: WebSocketListener(){
-    override fun onOpen(webSocket: WebSocket, response: Response)  {
-        Log.i("log", "onOpen")
-        println("onOpen")
-        println(response)
-    }
-
-    override fun onMessage(webSocket: WebSocket, response: String) {
-        Log.i("log", "onMessage")
-        println("onMessage")
-        println(response)
-    }
-
-    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-        super.onFailure(webSocket, t, response)
-        println(t.message)
-    }
-}
